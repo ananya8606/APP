c719de84b542b7a1aa365d02caf83783bdd0840e
ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
2023-12-05 16:17:20 +0530

    Update MST_Kruskals.c

--- MST_Kruskals.c
+++ MST_Kruskals.c
@@ -1,143 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Reference GFG and NPTEL
-//Kruskal’s Minimum Spanning Tree Algorithm
 
-struct Edge
-{
-	int src, dest, weight;
-};
-struct Graph
-{
-	int v,e;
-	struct Edge* edge;
-};
+#define MAX_VERTICES 100
 
 struct subset
 {
-	int parent;
-	int rank;
+    int parent;
+    int rank;
 };
 
-//Path compression
-int find(struct subset subsets[],int i)
+int find(struct subset subsets[], int i)
 {
-	if(subsets[i].parent!=i)
-		subsets[i].parent = find(subsets,subsets[i].parent);
+    if (subsets[i].parent != i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
 
-	return subsets[i].parent;
+    return subsets[i].parent;
 }
 
 void Union(struct subset subsets[], int x, int y)
 {
-	int xroot = find(subsets,x);
-	int yroot = find(subsets,y);
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
 
-	if(subsets[xroot].rank < subsets[yroot].rank)
-		subsets[xroot].parent = yroot;
-
-	else if(subsets[xroot].rank > subsets[yroot].rank)
-		subsets[yroot].parent = xroot;
-
-	else
-	{
-		subsets[yroot].parent = xroot;
-		subsets[xroot].rank++;
-	}
-
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+    else
+    {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
 }
 
 int scompare(const void *a, const void *b)
 {
-	// Compare function for qsort()
-	struct Edge* na = (struct Edge*)a;
-	struct Edge* nb = (struct Edge*)b;
+    struct Edge *na = (struct Edge *)a;
+    struct Edge *nb = (struct Edge *)b;
 
-	return na->weight > nb->weight;
+    return na->weight - nb->weight;
 }
 
-struct Graph* creategraph(int v,int e)
+void MST_kruskal(int v, int adjMatrix[MAX_VERTICES][MAX_VERTICES], int e)
 {
-	struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
-	graph->v = v;
-	graph->e = e;
-	graph->edge = (struct Edge*)malloc(sizeof(struct Edge)*e);
+    struct Edge result[v];
 
-	return graph;
-}
+    int edgeCount = 0;
+    int i = 0;
 
-void MST_kruskal(struct Graph* graph)
-{
-	int v = graph->v;
-	struct Edge result[v];
+    struct subset *subsets = (struct subset *)malloc(v * sizeof(struct subset));
 
-	int e = 0;
-	int i = 0;
+    for (i = 0; i < v; i++)
+    {
+        subsets[i].parent = i;
+        subsets[i].rank = 0;
+    }
+    i = 0;
 
-	// Sort edges according to their weight/length using library function
-	// qsort()- Quick Sort ~ O(mlogm).
+    // Create an array of edges for sorting
+    struct Edge *edges = (struct Edge *)malloc(e * sizeof(struct Edge));
 
-	qsort(graph->edge,graph->e,sizeof(graph->edge[0]),scompare);
+    for (int row = 0; row < v; row++)
+    {
+        for (int col = row; col < v; col++)
+        {
+            if (adjMatrix[row][col] != 0)
+            {
+                edges[i].src = row;
+                edges[i].dest = col;
+                edges[i].weight = adjMatrix[row][col];
+                i++;
+            }
+        }
+    }
 
-	struct subset *subsets = (struct subset*)malloc(sizeof(struct subset)*v);
+    // Sort edges according to their weight/length using library function qsort()
+    qsort(edges, e, sizeof(edges[0]), scompare);
 
-	for(i=0; i<v;i++)
-	{
-		// initializing all subsets containing only one element.
-		// and having rank 0 ie. only one node.
-		subsets[i].parent = i;
-		subsets[i].rank = 0;
-	}
-	i = 0;
-	
-	// As in a minimum spanning tree no of edged are n-1.
-	while(e < v-1)
-	{
-		struct Edge crawl = graph->edge[i++];
-		//checking every edge. 
+    // As in a minimum spanning tree, the number of edges is n-1.
+    while (edgeCount < v - 1)
+    {
+        struct Edge crawl = edges[i++];
 
-		int x = find(subsets,crawl.src);
-		int y = find(subsets,crawl.dest);
+        int x = find(subsets, crawl.src);
+        int y = find(subsets, crawl.dest);
 
-		if(x!=y)// Avoid cycles.
-		{
-			result[e++] = crawl;// adding edge to MST.
-			Union(subsets, x, y);
-		}
-	}
+        if (x != y) // Avoid cycles
+        {
+            result[edgeCount++] = crawl;
+            Union(subsets, x, y);
+        }
+    }
 
-	printf("Following edges are there in MST \n");
-	printf("src--dest=weight\n");
-	for(i=0; i<e; i++)
-		printf("%d--%d=%d\n",result[i].src,result[i].dest,result[i].weight);
+    printf("Following edges are there in MST\n");
+    printf("src--dest=weight\n");
+    for (i = 0; i < edgeCount; i++)
+        printf("%d--%d=%d\n", result[i].src, result[i].dest, result[i].weight);
 }
 
 int main()
 {
-	int v = 4,e=5;
-	struct Graph* graph = creategraph(v,e);
+    int v, e;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &v);
 
-	graph->edge[0].src = 0;
-	graph->edge[0].dest = 1;
-	graph->edge[0].weight = 10;
+    printf("Enter the number of edges: ");
+    scanf("%d", &e);
 
-	graph->edge[1].src = 0;
-	graph->edge[1].dest = 2;
-	graph->edge[1].weight = 6;
+    int adjMatrix[MAX_VERTICES][MAX_VERTICES] = {0};
 
-	graph->edge[2].src = 0;
-	graph->edge[2].dest = 3;
-	graph->edge[2].weight = 5;
+    printf("Enter edges with their weights (src dest weight):\n");
+    for (int i = 0; i < e; i++)
+    {
+        int src, dest, weight;
+        scanf("%d %d %d", &src, &dest, &weight);
+        adjMatrix[src][dest] = weight;
+        adjMatrix[dest][src] = weight;
+    }
 
-	graph->edge[3].src = 1;
-	graph->edge[3].dest = 3;
-	graph->edge[3].weight = 15;
+    MST_kruskal(v, adjMatrix, e);
 
-	graph->edge[4].src = 2;
-	graph->edge[4].dest = 3;
-	graph->edge[4].weight = 4;
-
-	MST_kruskal(graph);
-
-	return 0;
+    return 0;
 }
