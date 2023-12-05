97ff798396ee1a19aed556dc1f8d4f323434964b
ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
2023-12-05 16:26:46 +0530

    Update Ass_8_2.c

--- Assignment 8/Ass_8_2.c
+++ Assignment 8/Ass_8_2.c
@@ -4,80 +4,57 @@
 
 #define MAX_VERTICES 100
 
-struct Node {
-    int data;
-    struct Node* next;
-};
-
-struct Graph {
-    int vertices;
-    struct Node** adjacencyList;
-};
-
-struct Graph* createGraph(int vertices) {
-    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
-    graph->vertices = vertices;
-    graph->adjacencyList = (struct Node**)malloc((vertices+1) * sizeof(struct Node*));
-
-    for (int i = 1; i <= vertices; ++i) {
-        graph->adjacencyList[i] = NULL;
-    }
-
-    return graph;
+void addEdge(int vertices, int adjMatrix[vertices][vertices], int src, int dest) {
+    adjMatrix[src][dest] = 1;
+    adjMatrix[dest][src] = 1;
 }
 
-void addEdge(struct Graph* graph, int src, int dest) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = dest;
-    newNode->next = graph->adjacencyList[src];
-    graph->adjacencyList[src] = newNode;
-
-    newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = src;
-    newNode->next = graph->adjacencyList[dest];
-    graph->adjacencyList[dest] = newNode;
-}
-
-void DFS(struct Graph* graph, int vertex, bool visited[], int currentSet[]) {
+void DFS(int vertices, int adjMatrix[vertices][vertices], int vertex, bool visited[], int currentSet[]) {
     visited[vertex] = true;
     printf("%d ", vertex);
 
-    struct Node* current = graph->adjacencyList[vertex];
-    while (current != NULL) {
-        int neighbor = current->data;
-        if (!visited[neighbor]) {
+    for (int neighbor = 1; neighbor <= vertices; ++neighbor) {
+        if (adjMatrix[vertex][neighbor] == 1 && !visited[neighbor]) {
             currentSet[neighbor] = currentSet[vertex];
-            DFS(graph, neighbor, visited, currentSet);
+            DFS(vertices, adjMatrix, neighbor, visited, currentSet);
         }
-        current = current->next;
     }
 }
 
-void findDisjointSets(struct Graph* graph) {
+void findDisjointSets(int vertices, int adjMatrix[vertices][vertices]) {
     bool visited[MAX_VERTICES] = { false };
     int currentSet[MAX_VERTICES] = { 0 };
 
     int setNumber = 1;
 
-    for (int i = 1; i <= graph->vertices; ++i) {
+    for (int i = 1; i <= vertices; ++i) {
         if (!visited[i]) {
             printf("Set %d: ", setNumber++);
             currentSet[i] = i;
-            DFS(graph, i, visited, currentSet);
+            DFS(vertices, adjMatrix, i, visited, currentSet);
             printf("\n");
         }
     }
 }
 
 int main() {
-    struct Graph* graph = createGraph(8);
-    addEdge(graph, 3, 1);
-    addEdge(graph, 8, 2);
-    addEdge(graph, 6, 2);
-    addEdge(graph, 5, 6);
-    addEdge(graph, 4, 2);
-    addEdge(graph, 7, 7);
-    findDisjointSets(graph);
+    int vertices, edges;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &vertices);
+
+    int adjMatrix[MAX_VERTICES][MAX_VERTICES] = {0};
+
+    printf("Enter the number of edges: ");
+    scanf("%d", &edges);
+
+    printf("Enter edges (src dest):\n");
+    for (int i = 0; i < edges; i++) {
+        int src, dest;
+        scanf("%d %d", &src, &dest);
+        addEdge(vertices, adjMatrix, src, dest);
+    }
+
+    findDisjointSets(vertices, adjMatrix);
 
     return 0;
 }
