903ac111b66cb8e1ccd116754b93a4d64603ce83
ANANYA GUPTA <52853286+ananya8606@users.noreply.github.com>
2023-12-05 16:10:00 +0530

    Update KosarajusSCC.c

--- KosarajusSCC.c
+++ KosarajusSCC.c
@@ -1,181 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Kosaraju's algorithm for strongly connected components.
-struct Graph
-{
-	int v;
-	struct AdjList *array;
-};
-
-struct AdjList
-{
-	struct edge* head;
-};
-
-struct edge
-{
-	int dest;
-	struct edge* next;
-};
-
 struct Stack
 {
-	int data;
-	struct Stack* next;
+    int data;
+    struct Stack *next;
 };
 
-struct Stack* stack = NULL;
+struct Stack *stack = NULL;
 
 int top()
 {
-	if(stack == NULL)
-		return -1;
+    if (stack == NULL)
+        return -1;
 
-	return stack->data;
+    return stack->data;
 }
 
 void pop()
 {
-	if(stack==NULL)
-		return;
+    if (stack == NULL)
+        return;
 
-	struct Stack* node = stack;
-	stack = stack->next;
+    struct Stack *node = stack;
+    stack = stack->next;
 
-	free(node);
+    free(node);
 }
 
 int isEmpty()
 {
-	if(stack==NULL)
-		return 1;
-
-	else
-		return 0;
+    return stack == NULL ? 1 : 0;
 }
 
 void push(int data)
 {
-	struct Stack* node = (struct Stack*)malloc(sizeof(struct Stack));
-	node->data = data;
-	node->next = stack;
+    struct Stack *node = (struct Stack *)malloc(sizeof(struct Stack));
+    node->data = data;
+    node->next = stack;
 
-	stack = node;
+    stack = node;
 }
 
-struct Graph* createGraph(int v)
+void DFSutil(int v, int n, int visited[n], int graph[n][n])
 {
-	struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph)*v);
+    visited[v] = 1;
+    printf("%d ", v);
 
-	graph->v = v;
-	graph->array = (struct AdjList*)malloc(sizeof(struct AdjList)*v);
-
-	for(int i=0; i<graph->v;i++)
-		graph->array[i].head = NULL;
-
-	return graph;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i] && graph[v][i])
+        {
+            DFSutil(i, n, visited, graph);
+        }
+    }
 }
 
-void addEdge(struct Graph* graph, int src, int dest)
+void fillOrder(int v, int n, int visited[n], int graph[n][n])
 {
-	struct edge* newedge = (struct edge*)malloc(sizeof(struct edge));
-	newedge->dest = dest;
+    visited[v] = 1;
 
-	newedge->next = graph->array[src].head;
-	graph->array[src].head = newedge;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i] && graph[v][i])
+        {
+            fillOrder(i, n, visited, graph);
+        }
+    }
+
+    push(v);
 }
 
-struct Graph* Transpose(struct Graph* graph)
+void printSCC(int n, int graph[n][n])
 {
-	struct Graph* tgraph = createGraph(graph->v);
+    stack = NULL;
+    int visited[n];
 
-	for(int i=0; i< graph->v ;i++)
-	{
-		struct edge* crawl = NULL;
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
 
-		for(crawl = graph->array[i].head; crawl; crawl = crawl->next)
-		{
-			struct edge* newedge = (struct edge*)malloc(sizeof(struct edge));
-			newedge->dest = i;
-			newedge->next = tgraph->array[crawl->dest].head;
-			tgraph->array[crawl->dest].head = newedge;
-		}
-	}
-	return tgraph;
+    for (int i = 0; i < n; i++)
+        if (!visited[i])
+            fillOrder(i, n, visited, graph);
+
+    int tgraph[n][n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            tgraph[i][j] = graph[j][i];
+
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    while (!isEmpty())
+    {
+        int v = top();
+        pop();
+
+        if (!visited[v])
+        {
+            DFSutil(v, n, visited, tgraph);
+            printf("\n");
+        }
+    }
 }
 
-void DFSutil(struct Graph* graph,int v,int visited[])
+int main()
 {
-	visited[v] = 1;
-	printf("%d ",v);
+    int n, e;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
 
-	struct edge* crawl = NULL;
+    int graph[n][n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            graph[i][j] = 0;
 
-	for(crawl = graph->array[v].head; crawl; crawl = crawl->next)
-		if(!visited[crawl->dest])
-			DFSutil(graph,crawl->dest,visited);
-}
+    printf("Enter the number of edges: ");
+    scanf("%d", &e);
 
-void fillOrder(struct Graph* graph, int v, int visisted[])
-{
-	visisted[v] = 1;
-
-	struct edge* crawl = NULL;
-
-	for(crawl = graph->array[v].head; crawl; crawl = crawl->next)
-		if(!visisted[crawl->dest])
-			fillOrder(graph,crawl->dest,visisted);
-
-	push(v);
-}
-
-void printSCC(struct Graph* graph)
-{
-	stack = NULL;
-	int i;
-
-	int *visisted = (int *)malloc(sizeof(int)*graph->v);
-
-	for(i=0; i<graph->v; i++)
-		visisted[i] = 0;
-
-	for(i=0 ;i<graph->v; i++)
-		if(!visisted[i])
-			fillOrder(graph, i, visisted);
-
-	struct Graph* revgraph = Transpose(graph);
-
-	for(i=0; i<graph->v; i++)
-		visisted[i] = 0;
-
-	while(!isEmpty())
-	{
-		int v = top();
-		pop();
-
-		if(!visisted[v])
-		{
-			DFSutil(revgraph,v,visisted);
-			printf("\n");
-		}
-		
-	}
-}
-
-int main(int argc, char const *argv[])
-{
-	int v = 5;
-	struct Graph* graph = createGraph(v);
-
-	addEdge(graph,1, 0);
-    addEdge(graph,0, 2);
-    addEdge(graph,2, 1);
-    addEdge(graph,0, 3);
-    addEdge(graph,3, 4);
+    for (int i = 0; i < e; i++)
+    {
+        int src, dest;
+        printf("Enter edge %d (source destination): ", i + 1);
+        scanf("%d %d", &src, &dest);
+        graph[src][dest] = 1;
+    }
 
     printf("Following are the strongly connected components \n");
-    printSCC(graph);
-	
-	return 0;
+    printSCC(n, graph);
+
+    return 0;
 }
